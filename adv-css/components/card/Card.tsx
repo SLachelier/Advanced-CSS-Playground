d9f70c24920f9c44ae16f2952fdcd82ad7d22017
Shauna L <slachelier@gmail.com>
2024-07-02 14:09:58 -0500

    re-mapped file structure and created card component

---
+++ adv-css/components/card/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card-styles.css';
+
+export default function Card() {
+  return (
+    <>
+      <div>
+        <h2>This is a card</h2>
+      </div>
+    </>
+  );
+}
